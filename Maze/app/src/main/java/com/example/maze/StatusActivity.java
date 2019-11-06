30d644d804d4111542eb2d0395ca54adfc6017ef
AlphaPiece <twopieces0921@gmail.com>
2019-11-06 00:01:36 -0500

    Added dead status activity

--- Maze/app/src/main/java/com/example/maze/StatusActivity.java
+++ Maze/app/src/main/java/com/example/maze/StatusActivity.java
@@ -2,7 +2,10 @@ package com.example.maze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class StatusActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class StatusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+        new CountDownTimer(4000, 1000) {
+
+            TextView statusTimer = findViewById(R.id.statusTimer);
+
+            public void onTick(long millisUntilFinished) {
+                statusTimer.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                Intent intent = new Intent(StatusActivity.this, ModeActivity.class);
+                startActivity(intent);
+            }
+
+        }.start();
     }
 }
